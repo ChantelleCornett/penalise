9cc53ef4c090c5c051b7f996d981c19086824cf7
ChantelleCornett <146009783+ChantelleCornett@users.noreply.github.com>
2023-11-14 13:21:53 +0000

    Update compMethodsSim.r

--- compMethodsSim.r
+++ compMethodsSim.r
@@ -20,8 +20,8 @@
 ##.    * Gender (DONE)                                                      ##
 ##.    * Year of birth (DONE)                                               ##
 ##.    * Socioeconomic status (DONE)                                        ##
-##.    * Region                                                             ##
-##.    * Event info                                                         ##
+##.    * Region (DONE)                                                      ##
+##.    * Event info (date, type)                                            ##
 ##############################################################################
 
 ######################################
@@ -72,5 +72,60 @@ ses <- as.list(sample(c(1,2,3,4,5), size = length(patid), replace = TRUE,
 region <- as.list(sample(seq(1,13,1), size = length(patid), replace = TRUE))
 
 
+########### Events ##################
 
+# Set the seed for reproducibility
+set.seed(123)
+
+# Function to simulate a multi-state illness-death model
+simulate_multi_state <- function(n, time_max) {
+  time <- rep(NA, n)
+  state <- rep(NA, n)
+  
+  for (i in 1:n) {
+    t <- 0
+    current_state <- 1  # Start in state 1 (e.g., Healthy)
+    
+    while (t < time_max) {
+      # Generate time until the next event
+      delta_t <- rexp(1, rate = -Q[current_state, current_state])
+      
+      # Update time and state
+      t <- t + delta_t
+      time[i] <- t
+      state[i] <- current_state
+      
+      # Check for death event
+      if (current_state == n_states) {
+        break
+      }
+      
+      # Generate next state
+      u <- runif(1)
+      prob_transitions <- cumsum(Q[current_state, ] / -Q[current_state,
+                                                         current_state])
+      next_state <- sum(u > prob_transitions) + 1
+      
+      current_state <- next_state
+    }
+  }
+  
+  return(data.frame(time = time, state = state))
+}
+
+# Define the number of individuals and number of states
+n_individuals <- 2000
+n_states <- 3
+
+# Define the transition intensity matrix
+Q <- matrix(c(-0.1, 0.1, 0, 0, -0.2, 0.2, 0, 0, -0.05), nrow = n_states,
+            byrow = TRUE)
+
+# Simulate data
+sim_data <- simulate_multi_state(n = n_individuals, time_max = 10)
+
+# Plot the simulated data
+plot(sim_data$time, sim_data$state, type = 's', col = 'blue', xlab = 'Time',
+     ylab = 'State',
+     main = 'Simulated Multi-State Illness-Death Model')
 
