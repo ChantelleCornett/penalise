0c96bc661e67ce9671f6434c0b654738ed9b4a06
ChantelleCornett <146009783+ChantelleCornett@users.noreply.github.com>
2023-11-28 10:26:49 +0000

    Bayesian method applied

--- PenaliseMeth.R
+++ PenaliseMeth.R
@@ -1,6 +1,6 @@
 ##############################################################################
 ##               Comparative methods study on Penalisation                  ##
-##                    Methods for Multi-State Models                        ##  
+##                    Methods for Multi-State Models                        ##
 ##                              Penalisation                                ##
 ##                       Author: Chantelle Cornett                          ##
 ##                           Date: 19NOV2023                                ##
@@ -32,73 +32,139 @@ msdat$event <- msdat$status
 ###################################
 
 # make a cox proportional hazards model with multiple outcomes
-noShrinkSimp <- coxph(Surv(entry, exit, event) ~ strata(trans) + gender*factor(trans) + age*factor(trans)+ BMI*factor(trans),
-      data = msdat,
-      method = "breslow")
+noShrinkSimp <-
+  coxph(
+    Surv(entry, exit, event) ~ strata(trans) + gender * factor(trans) + age *
+      factor(trans) + BMI * factor(trans),
+    data = msdat,
+    method = "breslow",
+    x = TRUE
+  )
 
 ###################################
 ##     UNIFORM SHRINKAGE         ##
 ###################################
 
-# using parallel processing
-cl <- makeCluster(5)
-registerDoParallel(5)
-start_time_fit_parallel <- Sys.time()
+options(warn=1)
+uniShrinkSimp <-
+  shrink.coxph(
+    fit = noShrinkSimp,
+    type = "global",
+    join = NULL,
+    method = "jackknife",
+    postfit = TRUE,
+    notes = TRUE
+  )
 
-uniSimp_list<-(foreach(input=1:5, .combine=list, .multicombine=TRUE, 
-                             .packages=c("shrink", "mstate", "survival"))) %dopar%{
-uniShrinkSimp <- shrink.coxph(fit = noShrinkSimp, type = "global", method = "jackknife", join = NULL, postfit = FALSE)
-                             }
 
-diff_fit_parallel <- start_time_fit_parallel - end_time_fit_parallel
-stopCluster(cl)
-print("FINISH DATA GEN")
-diff_fit_parallel
 ###################################
 ##  LASSO PENALISED LIKELIHOOD   ##
 ###################################
 
 # PRE-PROCESSING DATA
-d <- msdat[, c("entry", "exit","trans","event")]
-X <- msdat[, c("trans","gender","age","BMI")]
+d <- msdat[, c("entry", "exit", "trans", "event")]
+X <- msdat[, c("trans", "gender", "age", "BMI")]
 
 # 10-FOLD CROSS VALIDATION FOR OPTIMAL VALUE OF LAMBDA
 xmat <- as.matrix(X)
-ysurv <- msdat[, c("Tstop","status")]
+ysurv <- msdat[, c("Tstop", "status")]
 cvfit <- cv.glmnet(xmat, ysurv, family = "cox", type.measure = "C")
-lassoSimp <- penMSM(type = "lasso", d, X, PSM1, PSM2, lambda1, lambda2, w, betastart, nu = 0.5, tol = 1e-10,
-       max.iter = 50, trace = TRUE, diagnostics = TRUE, family = "coxph", constant.approx = 1e-8)
+lassoSimp <-
+  penMSM(
+    type = "lasso",
+    d,
+    X,
+    PSM1,
+    PSM2,
+    lambda1,
+    lambda2,
+    w,
+    betastart,
+    nu = 0.5,
+    tol = 1e-10,
+    max.iter = 50,
+    trace = TRUE,
+    diagnostics = TRUE,
+    family = "coxph",
+    constant.approx = 1e-8
+  )
 
 ##########################################
 ##   FUSED LASSO PENALISED LIKELIHOOD   ##
 ##########################################
 
-fusedSimp <- penMSM(type = "fused", d, X, PSM1, PSM2, lambda1, lambda2, w, betastart, nu = 0.5, tol = 1e-10,
-                    max.iter = 50, trace = TRUE, diagnostics = TRUE, family = "coxph", poissonresponse = NULL,
-                    poissonoffset = NULL, constant.approx = 1e-8)
+fusedSimp <-
+  penMSM(
+    type = "fused",
+    d,
+    X,
+    PSM1,
+    PSM2,
+    lambda1,
+    lambda2,
+    w,
+    betastart,
+    nu = 0.5,
+    tol = 1e-10,
+    max.iter = 50,
+    trace = TRUE,
+    diagnostics = TRUE,
+    family = "coxph",
+    poissonresponse = NULL,
+    poissonoffset = NULL,
+    constant.approx = 1e-8
+  )
 
 ###################################
 ##      REDUCED RANK METHOD      ##
 ###################################
 
 # The reduced rank 2 solution
-rr2 <- myredrank(Surv(Tstart, Tstop, status) ~ strata(trans) + gender*factor(trans) + age*factor(trans)+ BMI*factor(trans),
-               data=msdat, R=2)
-rr2$Alpha; rr2$Gamma; rr2$Beta; rr2$loglik
+rr2 <-
+  redrank(
+    Surv(Tstart, Tstop, status) ~ strata(trans) + gender * factor(trans) + age *
+      factor(trans) + BMI * factor(trans),
+    data = msdat,
+    R = 2
+  )
+rr2$Alpha
+rr2$Gamma
+rr2$Beta
+rr2$loglik
 
 # The full rank solution
-rr3 <- myredrank(Surv(entry, exit, event) ~ strata(trans) + genders*factor(trans) + ages*factor(trans),
-               data=msdat, R=3)
-rr3$Alpha; rr3$Gamma; rr3$Beta; rr3$loglik
+rr3 <-
+  myredrank(
+    Surv(entry, exit, event) ~ strata(trans) + genders * factor(trans) + ages *
+      factor(trans),
+    data = msdat,
+    R = 3
+  )
+rr3$Alpha
+rr3$Gamma
+rr3$Beta
+rr3$loglik
 
 ###################################
 ##      BAYESIAN APPROACH        ##
 ###################################
+model_covshrink <- bf(exit| cens(1 - status) ~ base + cand,
+                      base ~ 1,
+                      cand ~ strata(trans) + gender * factor(trans) + age *
+                        factor(trans),
+                      nl=TRUE)
+get_prior(model_covshrink, data=msdat)
 
-fit <- brm(Surv(entry, exit, event) ~ strata(trans) + gender*factor(trans) + age*factor(trans)+BMI*factor(trans),
-           data = msdat,
-           family = brms::cox(),
-           chains = 2,
-           warmup = 1000,
-           seed=123,
-           prior = set_prior("horseshoe(0.5)", class = "b"))
+prior_covshrink <- prior(normal(0,2), coef="Intercept", nlpar="base") +
+  prior(horseshoe(df=3, scale_global=2, df_global=6, scale_slab=1, df_slab=6), class="b", nlpar="cand")
+
+fit <-
+  brm(
+    model_covshrink,
+    data = msdat,
+    family = brms::cox(),
+    chains = 2,
+    warmup = 10,
+    seed = 123,
+    prior = prior_covshrink
+  )
