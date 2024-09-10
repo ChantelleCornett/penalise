29379f6876049b5ab8259765f923898335592dcd
Chantelle Cornett <146009783+ChantelleCornett@users.noreply.github.com>
2024-09-10 11:42:14 +0100

    Add files via upload

---
+++ realHorseshoe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash --login
+
+
+#$ -pe smp.pe 32
+
+module load tools/gcc/cmake/3.23.0
+module load libs/gcc/nlopt/2.6.2
+module load apps/gcc/R/4.3.1
+
+Rscript "/mnt/iusers01/ja01/m13477cc/scratch/realHorseshoe.R"
